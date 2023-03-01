1b6a9055f39a26d77b573c6cdd6a599cbbe9ddad
Danai <danaefil@outlook.com>
2023-03-01 16:14:32 +0100

    create logger sketch file

---
+++ logger_sketch/logger_sketch.ino
@@ -0,0 +1,19 @@
+/CourseElectronics
+ Author:Danai Filippou
+ Date:01/03/2023
+
+#include <SD.h>
+#include <SPI.h>
+#include <RTClib.h>
+#include <OneWire.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
